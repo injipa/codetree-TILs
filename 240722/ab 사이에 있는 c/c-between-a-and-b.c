fb4b203b2c0b651a0af37b8c7f72251715027350
injipa <qhtjr1113@ewha.ac.kr>
2024-07-22 17:10:54 +0900

    Title: ab 사이에 있는 c, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240722/ab 사이에 있는 c/c-between-a-and-b.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int a, b, c;
+    bool satisfied=false;
+
+    scanf("%d%d%d", &a, &b, &c);
+    for(int i=a; i<=b; i++){
+        if(i%c==0)
+        satisfied=true;
+    }
+        if(satisfied==true){
+            printf("YES");
+        }
+        else{
+            printf("NO");
+        }
+
+    return 0;
+}
