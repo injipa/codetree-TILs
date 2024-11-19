c74ad431b8c9695583f56555a4987279ae34a098
injipa <qhtjr1113@ewha.ac.kr>
2024-11-19 13:26:28 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241119/반복 출력하기/repeated-output.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void Print(int n){
+    for(int i=0; i<n; i++)
+    printf("12345^&*()_\n");
+}
+
+int main() {
+    int i;
+    scanf("%d", &i);
+
+    Print(i);
+    
+    return 0;
+}
