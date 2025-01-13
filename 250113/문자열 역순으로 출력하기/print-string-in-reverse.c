fae5fb92cb8d1998ef25eac6fda938901d0e84da
injipa <qhtjr1113@ewha.ac.kr>
2025-01-13 13:26:41 +0900

    Title: 문자열 역순으로 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250113/문자열 역순으로 출력하기/print-string-in-reverse.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char str[4][21];
+    
+    for(int i=0; i<4; i++){
+        scanf("%s", str[i]);
+    }
+     for(int i=3; i>=0; i--){
+        printf("%s\n", str[i]);
+    }
+    return 0;
+}
