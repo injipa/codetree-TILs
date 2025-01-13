7bc574da63cb21c86d62202a3fa8075b17fb8fa6
injipa <qhtjr1113@ewha.ac.kr>
2025-01-13 14:43:58 +0900

    Title: 특정 문자로 시작하는 문자열, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 250113/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    int a;
+    char str[10][21];
+    char b;
+    int cnt=0;
+    int len=0;
+
+    scanf("%d", &a);
+    for(int i=0; i<a; i++){
+        scanf("%s", str[i]);
+    }
+    getchar();
+    scanf("%c", &b);
+
+    for(int i=0; i<a; i++){
+        if(str[i][0]==b){
+            cnt++;
+            len+=strlen(str[i]);
+        }
+    }
+
+    printf("%d %.2lf", cnt, (double)len/cnt);
+
+
+
+    return 0;
+}
