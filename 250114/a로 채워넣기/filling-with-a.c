e3a7a60db77e2133f1d2f0e4aca3bedbcf28fefb
injipa <qhtjr1113@ewha.ac.kr>
2025-01-14 17:34:59 +0900

    Title: a로 채워넣기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250114/a로 채워넣기/filling-with-a.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[101];
+    scanf("%s", str);
+
+    str[1]='a';
+    str[strlen(str)-2]='a';
+
+    printf("%s", str);
+
+    return 0;
+}
