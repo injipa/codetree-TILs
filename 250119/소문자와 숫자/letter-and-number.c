ddda01fd63a823c853e514486b3935fe80b010a6
injipa <qhtjr1113@ewha.ac.kr>
2025-01-19 16:44:03 +0900

    Title: 소문자와 숫자, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250119/소문자와 숫자/letter-and-number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char str [101];
+    scanf("%s", str);
+
+    for(int i=0; str[i]!='\0'; i++){
+        if(('A'<=str[i]&&str[i]<='z')||('a'<=str[i]&&str[i]<='z'))
+        {
+            printf("%c", tolower(str[i]));
+        }
+        else if('0'<=str[i]&&str[i]<='9'){
+            printf("%c", str[i]);
+        }
+    }
+
+    return 0;
+}
