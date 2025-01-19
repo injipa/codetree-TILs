01d95a4afd65a7f25d0bfc54c7b96660ea27bc1d
injipa <qhtjr1113@ewha.ac.kr>
2025-01-19 16:47:18 +0900

    Title: 소문자와 숫자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250119/소문자와 숫자/letter-and-number.c
+++ 250119/소문자와 숫자/letter-and-number.c
@@ -4,13 +4,7 @@
 int main() {
     char str [101];
     scanf("%s", str);
-
-        for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] == '\n') {
-            str[i] = '\0'; // 개행문자 제거
-            break;
-        }
-    }
+    
 
     for(int i=0; str[i]!='\0'; i++){
         if(('A'<=str[i]&&str[i]<='z')||('a'<=str[i]&&str[i]<='z'))
