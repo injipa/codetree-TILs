0515d6c7381d6de75234bc00cf7eb0df5d626d0f
injipa <qhtjr1113@ewha.ac.kr>
2025-01-19 16:08:26 +0900

    Title: 아스키코드표 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250119/아스키코드표 맞추기/chart-of-ASCII.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c, d, e;
+    scanf("%d%d%d%d%d", &a, &b, &c, &d, &e);
+
+    printf("%c %c %c %c %c", (char)a, (char)b, (char)c, (char)d, (char)e);
+
+    return 0;
+}
